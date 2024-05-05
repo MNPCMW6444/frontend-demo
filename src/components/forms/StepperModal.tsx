997bc6862f1ce1c399a6fce1e9ffb465c4805ebc
GitHub Action <michael+cicd@offisito.com>
2024-05-05 13:52:46 +0300

    temp

--- src/components/forms/StepperModal.tsx
+++ src/components/forms/StepperModal.tsx
@@ -3,29 +3,49 @@ import {
     DialogActions,
     DialogContent,
     DialogTitle,
-    Grid,
+    Grid, Typography,
 } from "@mui/material";
 import {PrimaryButton} from "../hoc";
-import {ReactNode, useState} from "react";
+import {ReactNode, useCallback, useMemo, useState} from "react";
+import nameUI from "../steps/nameUI.tsx";
+import ageUI from "../steps/ageUI.tsx";
 
-export  type StepState<D> = (D & { invalid: boolean })
+export type RenderStepFunctionType<D> = (stepState: D | undefined, setStepState: (newState: D) => void, setIsInvalid: (isInvalid: boolean) => void) => ReactNode
+
+const steps: RenderStepFunctionType<any>[] = [
+    nameUI,
+    ageUI
+]
 
 interface StepperModalProps {
     closeModalFunction: () => void;
 }
 
 const StepperModal = ({closeModalFunction}: StepperModalProps) => {
-    const [formSate, setFormSate] = useState()
-    const [steps, setSteps] = useState<{ stepState: StepState<unknown>, UI: ReactNode }[]>()
-
     const totalSteps = steps.length + 1
-    const validateStep = (input: number): boolean => (input > -1 && input <= totalSteps)
+
+    const [formState, setFormState] = useState<any[]>(Array(steps.length))
 
     const [step, setStep] = useState<number>(0)
+    const validateStep = (input: number): boolean => (input > -1 && input <= totalSteps)
+
+    const stepState = useMemo(() => formState[step], [formState, step])
+    const setStepState = useCallback((newState: any) => {
+        setFormState(prev => {
+            const updatedState = [...prev];
+            updatedState[step] = newState;
+            return updatedState;
+        });
+    }, [setFormState, step])
+
+    const [isCurrentStepInvalid, setIsCurrentStepInvalid] = useState(true)
 
     const moveBackwards = () => setStep(prevStep => prevStep - 1)
     const moveForwards = () => setStep(prevStep => prevStep + 1)
-    const finish = () => closeModalFunction()
+    const finish = () => {
+        localStorage.setItem('data', JSON.stringify(formState));
+        closeModalFunction()
+    }
 
     const currentStep = steps[step]
     return <Dialog
@@ -37,7 +57,9 @@ const StepperModal = ({closeModalFunction}: StepperModalProps) => {
     >
         <DialogTitle>Tell us about yourself</DialogTitle>
         <DialogContent>
-            {currentStep.UI}
+            {step === totalSteps - 1 ?
+                <Typography>Stringified Summery: {JSON.stringify(formState)}</Typography>
+                : currentStep(stepState, setStepState, (isInvalid) => setIsCurrentStepInvalid(isInvalid))}
         </DialogContent>
         <DialogActions>
             <Grid container justifyContent="space-around">
@@ -47,7 +69,7 @@ const StepperModal = ({closeModalFunction}: StepperModalProps) => {
                 </Grid>
                 {step !== totalSteps - 1 ?
                     <Grid item>
-                        <PrimaryButton disabled={(validateStep(step + 1) === false) || currentStep.stepState.invalid}
+                        <PrimaryButton disabled={(validateStep(step + 1) === false) || isCurrentStepInvalid}
                                        onClick={moveForwards}>Next</PrimaryButton>
                     </Grid>
                     : <Grid item>
--- src/components/steps/nameUI.tsx
+++ src/components/steps/nameUI.tsx
@@ -1,10 +1,29 @@
+import {RenderStepFunctionType} from "../forms/StepperModal.tsx";
+import {Grid, TextField} from "@mui/material";
+import {useEffect} from "react";
+
 interface NameStepState {
-    fn:string;
-    ln:string
+    firstName?: string;
+    lastName?: string
 }
 
-export  default   (stepState:NameStepState, setStepState:(newState:NameStepState)=>void,setIsInvalid(isInvalid:boolean)=>void)=>{
+const renderNameUI: RenderStepFunctionType<NameStepState> = (stepState, setStepState, setIsInvalid) => {
+    useEffect(() => {
+        setIsInvalid(!stepState?.firstName || !stepState?.lastName)
+    }, [stepState]);
 
-return <></>
-
+    return <Grid container direction="column" rowSpacing={2}>
+        <Grid item>
+            <TextField label="First Name" value={stepState?.firstName || ""}
+                       onChange={(e) => setStepState({...(stepState || {}), firstName: e.target.value})}/>
+        </Grid>
+        <Grid item>
+            <TextField label="Last Name" value={stepState?.lastName || ""}
+                       onChange={(e) => setStepState({...(stepState || {}), lastName: e.target.value})}/>
+        </Grid>
+    </Grid>
 }
+
+export default renderNameUI
+
+
